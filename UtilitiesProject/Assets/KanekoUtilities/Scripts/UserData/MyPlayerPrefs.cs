1a99255a8075ddd3d526260e0d58febcd8f4448a
Kazuki <k.kaneko0311@gmail.com>
2018-07-06 14:41:00 +0900

    PlayerPrefsを更新

--- UtilitiesProject/Assets/KanekoUtilities/Scripts/UserData/MyPlayerPrefs.cs
+++ UtilitiesProject/Assets/KanekoUtilities/Scripts/UserData/MyPlayerPrefs.cs
@@ -1,7 +1,11 @@
-﻿using System.Collections;
+﻿//#define INPORT_HYPERCOMMON
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using HyperCasual;
+#if INPORT_HYPERCOMMON
+using HyperCasual;
+#endif
 
 namespace KanekoUtilities
 {
@@ -14,80 +18,128 @@ namespace KanekoUtilities
     {
         public static void SaveInt(SaveKeyName key, int value)
         {
-            PlayerPrefs.SetInt(key.ToString(), value);
+            SaveInt(key.ToString(), value);
         }
         public static void SaveString(SaveKeyName key, string value)
         {
-            PlayerPrefs.SetString(key.ToString(), value);
+            SaveString(key.ToString(), value);
         }
         public static void SaveFloat(SaveKeyName key, float value)
         {
-            PlayerPrefs.SetFloat(key.ToString(), value);
+            SaveFloat(key.ToString(), value);
         }
         public static void SaveBool(SaveKeyName key, bool value)
         {
-            PlayerPrefs.SetInt(key.ToString(), value ? 1 : 0);
+            SaveBool(key.ToString(), value);
         }
 
         public static int LoadInt(SaveKeyName key, int defaultValue = 0)
         {
-            return PlayerPrefs.GetInt(key.ToString(), defaultValue);
+            return LoadInt(key.ToString(), defaultValue);
         }
         public static string LoadString(SaveKeyName key, string defaultValue = "")
         {
-            return PlayerPrefs.GetString(key.ToString(), defaultValue);
+            return LoadString(key.ToString(), defaultValue);
         }
         public static float LoadFloat(SaveKeyName key, float defaultValue = 0.0f)
         {
-            return PlayerPrefs.GetFloat(key.ToString(), defaultValue);
+            return LoadFloat(key.ToString(), defaultValue);
         }
         public static bool LoadBool(SaveKeyName key, bool defaultValue = false)
         {
-            return PlayerPrefs.GetInt(key.ToString(), defaultValue ? 1 : 0) == 1;
+            return LoadBool(key.ToString(), defaultValue);
         }
 
         public static void Delete(SaveKeyName key)
         {
-            PlayerPrefs.DeleteKey(key.ToString());
+            Delete(key.ToString());
+        }
+
+#if !INPORT_HYPERCOMMON
+        public static void SaveInt(string key, int value)
+        {
+            PlayerPrefs.SetInt(key, value);
+        }
+        public static void SaveString(string key, string value)
+        {
+            PlayerPrefs.SetString(key, value);
+        }
+        public static void SaveFloat(string key, float value)
+        {
+            PlayerPrefs.SetFloat(key, value);
+        }
+        public static void SaveBool(string key, bool value)
+        {
+            PlayerPrefs.SetInt(key, value ? 1 : 0);
+        }
+
+        public static int LoadInt(string key, int defaultValue = 0)
+        {
+            return PlayerPrefs.GetInt(key, defaultValue);
+        }
+        public static string LoadString(string key, string defaultValue = "")
+        {
+            return PlayerPrefs.GetString(key, defaultValue);
+        }
+        public static float LoadFloat(string key, float defaultValue = 0.0f)
+        {
+            return PlayerPrefs.GetFloat(key, defaultValue);
+        }
+        public static bool LoadBool(string key, bool defaultValue = false)
+        {
+            return PlayerPrefs.GetInt(key, defaultValue ? 1 : 0) == 1;
+        }
+        
+        public static void Delete(string key)
+        {
+            PlayerPrefs.DeleteKey(key);
         }
         public static void DeleteAll()
         {
             PlayerPrefs.DeleteAll();
         }
+#else
+        public static void SaveInt(string key, int value)
+        {
+            IOUtility.PlayerPrefs.SaveEncryptedInt(key, value);
+        }
+        public static void SaveString(string key, string value)
+        {
+            IOUtility.PlayerPrefs.SaveEncryptedString(key, value);
+        }
+        public static void SaveFloat(string key, float value)
+        {
+            IOUtility.PlayerPrefs.SaveEncryptedFloat(key, value);
+        }
+        public static void SaveBool(string key, bool value)
+        {
+            IOUtility.PlayerPrefs.SaveEncryptedBool(key, value);
+        }
+        public static int LoadInt(string key, int defaultValue = 0)
+        {
+            return IOUtility.PlayerPrefs.LoadEncryptedInt(key, defaultValue);
+        }
+        public static string LoadString(string key, string defaultValue = "")
+        {
+            return IOUtility.PlayerPrefs.LoadEncryptedString(key, defaultValue);
+        }
+        public static float LoadFloat(string key, float defaultValue = 0.0f)
+        {
+            return IOUtility.PlayerPrefs.LoadEncryptedFloat(key, defaultValue);
+        }
+        public static bool LoadBool(string key, bool defaultValue = false)
+        {
+            return IOUtility.PlayerPrefs.LoadEncryptedBool(key, defaultValue);
+        }
         
-        /*// HyperCommonを導入したらこちらをコメントアウトして使う
-        public static void SaveInt(SaveKeyName key, int value)
+        public static void Delete(string key)
         {
-            IOUtility.PlayerPrefs.SaveEncryptedInt(key.ToString(), value);
+            IOUtility.PlayerPrefs.DeleteEncryptedKey(key);
         }
-        public static void SaveString(SaveKeyName key, string value)
+        public static void DeleteAll()
         {
-            IOUtility.PlayerPrefs.SaveEncryptedString(key.ToString(), value);
+            IOUtility.PlayerPrefs.DeleteAll();
         }
-        public static void SaveFloat(SaveKeyName key, float value)
-        {
-            IOUtility.PlayerPrefs.SaveEncryptedFloat(key.ToString(), value);
-        }
-        public static void SaveBool(SaveKeyName key, bool value)
-        {
-            IOUtility.PlayerPrefs.SaveEncryptedBool(key.ToString(), value);
-        }
-        public static int LoadInt(SaveKeyName key, int defaultValue = 0)
-        {
-            return IOUtility.PlayerPrefs.LoadEncryptedInt(key.ToString(), defaultValue);
-        }
-        public static string LoadString(SaveKeyName key, string defaultValue = "")
-        {
-            return IOUtility.PlayerPrefs.LoadEncryptedString(key.ToString(), defaultValue);
-        }
-        public static float LoadFloat(SaveKeyName key, float defaultValue = 0.0f)
-        {
-            return IOUtility.PlayerPrefs.LoadEncryptedFloat(key.ToString(), defaultValue);
-        }
-        public static bool LoadBool(SaveKeyName key, bool defaultValue = false)
-        {
-            return IOUtility.PlayerPrefs.LoadEncryptedBool(key.ToString(), defaultValue);
-        }
-        */
+#endif
     }
 }
---
+++ UtilitiesProject/Assets/KanekoUtilities/Scripts/UserData/RegisterParameter.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace KanekoUtilities
+{
+    public abstract class RegisterParameter<T>
+    {
+        protected string key;
+        protected T value;
+
+        public RegisterParameter(string key, T defaultValue)
+        {
+            this.key = key;
+            value = LoadValue(defaultValue);
+        }
+        public RegisterParameter(SaveKeyName key, T defaultValue)
+        {
+            this.key = key.ToString();
+            value = LoadValue(defaultValue);
+        }
+
+        public T GetValue() { return value; }
+        protected abstract T LoadValue(T defaultValue);
+        public abstract void SetValue(T value);
+    }
+
+    public class RegisterIntParameter : RegisterParameter<int>
+    {
+        public RegisterIntParameter(string key, int defaultValue) : base(key, defaultValue) { }
+        public RegisterIntParameter(SaveKeyName key, int defaultValue) : base(key, defaultValue) { }
+
+        protected override int LoadValue(int defaultValue)
+        {
+            return MyPlayerPrefs.LoadInt(key, defaultValue);
+        }
+
+        public override void SetValue(int value)
+        {
+            MyPlayerPrefs.SaveInt(key, value);
+        }
+    }
+
+    public class RegisterFloatParameter : RegisterParameter<float>
+    {
+        public RegisterFloatParameter(string key, float defaultValue) : base(key, defaultValue) { }
+        public RegisterFloatParameter(SaveKeyName key, float defaultValue) : base(key, defaultValue) { }
+
+        protected override float LoadValue(float defaultValue)
+        {
+            return MyPlayerPrefs.LoadFloat(key, defaultValue);
+        }
+
+        public override void SetValue(float value)
+        {
+            MyPlayerPrefs.SaveFloat(key, value);
+        }
+    }
+
+    public class RegisterStringParameter : RegisterParameter<string>
+    {
+        public RegisterStringParameter(string key, string defaultValue) : base(key, defaultValue) { }
+        public RegisterStringParameter(SaveKeyName key, string defaultValue) : base(key, defaultValue) { }
+
+        protected override string LoadValue(string defaultValue)
+        {
+            return MyPlayerPrefs.LoadString(key, defaultValue);
+        }
+
+        public override void SetValue(string value)
+        {
+            MyPlayerPrefs.SaveString(key, value);
+        }
+    }
+
+    public class RegisterBoolParameter : RegisterParameter<bool>
+    {
+        public RegisterBoolParameter(string key, bool defaultValue) : base(key, defaultValue) { }
+        public RegisterBoolParameter(SaveKeyName key, bool defaultValue) : base(key, defaultValue) { }
+
+        protected override bool LoadValue(bool defaultValue)
+        {
+            return MyPlayerPrefs.LoadBool(key, defaultValue);
+        }
+
+        public override void SetValue(bool value)
+        {
+            MyPlayerPrefs.SaveBool(key, value);
+        }
+    }
+}

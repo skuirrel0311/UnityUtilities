3929fbdaeeb5553e6ed64f9d99b3a047c56e3745
skuirrel0311 <rureikus7223@gmail.com>
2018-07-22 16:39:41 +0900

    画像素材修正

--- UtilitiesProject/Assets/KanekoUtilities/Scripts/UserData/Mission/MissionDialogElementPool.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace KanekoUtilities
-{
-    public class MissionDialogElementPool : ObjectPool<MissionDialogElement> { }
-}

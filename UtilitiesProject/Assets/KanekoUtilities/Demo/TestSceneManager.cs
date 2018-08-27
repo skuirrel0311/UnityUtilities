8280380799ee6e416c2321dd74d7d2a4481b175f
skuirrel0311 <rureikus7223@gmail.com>
2018-08-28 03:19:22 +0900

    音複数なっても大丈夫なようにした

--- UtilitiesProject/Assets/KanekoUtilities/Demo/TestSceneManager.cs
+++ UtilitiesProject/Assets/KanekoUtilities/Demo/TestSceneManager.cs
@@ -6,4 +6,39 @@ using KanekoUtilities;
 
 public class TestSceneManager : MonoBehaviour
 {
+    [SerializeField]
+    int count = 10;
+    [SerializeField]
+    int max = 50;
+
+    float t;
+
+    float interval = 0.2f;
+
+    [SerializeField]
+    SoundEffectPool soundEffectPool = null;
+    [SerializeField]
+    AudioClip clip = null;
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            t += Time.deltaTime;
+            if (t < interval) return;
+
+            t = 0.0f;
+
+            for (int i = 0; i < count; i++)
+            {
+                if (soundEffectPool.ActiveInstanceList.Count > max)
+                {
+                    return;
+                }
+
+                SoundEffect effect = soundEffectPool.GetInstance();
+                effect.Play(clip, () => soundEffectPool.ReturnInstance(effect));
+            }
+        }
+    }
 }

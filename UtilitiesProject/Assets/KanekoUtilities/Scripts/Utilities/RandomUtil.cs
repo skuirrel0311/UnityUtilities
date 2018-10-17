83676b6ea2312b08d5623aeb18847aa459335801
Kazuki <k.kaneko0311@gmail.com>
2018-10-17 10:46:03 +0900

    UnityRandomちょっとラップ

--- UtilitiesProject/Assets/KanekoUtilities/Scripts/Utilities/RandomUtil.cs
+++ UtilitiesProject/Assets/KanekoUtilities/Scripts/Utilities/RandomUtil.cs
@@ -1,9 +1,13 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using URandom;
 
 namespace KanekoUtilities
 {
+    /// <summary>
+    /// UnityRandomも使える（Seedが指定したい場合は自分で定義）
+    /// </summary>
     public static class RandomUtil
     {
         /// <summary>
@@ -11,7 +15,7 @@ namespace KanekoUtilities
         /// </summary>
         public static bool GetRandomBool(int probability)
         {
-            return Random.Range(0, 100) < probability;
+            return UnityEngine.Random.Range(0, 100) < probability;
         }
 
         /// <summary>
@@ -19,7 +23,7 @@ namespace KanekoUtilities
         /// </summary>
         public static T GetRandomValue<T>(T[] values)
         {
-            return values[Random.Range(0, values.Length)];
+            return values[UnityEngine.Random.Range(0, values.Length)];
         }
 
         /// <summary>
@@ -33,7 +37,7 @@ namespace KanekoUtilities
                 totalWeight += weightArray[i];
             }
 
-            int randomValue = Random.Range(1, totalWeight + 1);
+            int randomValue = UnityEngine.Random.Range(1, totalWeight + 1);
             int index = -1;
             for (var i = 0; i < weightArray.Length; ++i)
             {
@@ -46,5 +50,260 @@ namespace KanekoUtilities
             }
             return index;
         }
+
+        #region UnityRandom
+
+        static UnityRandom urand;
+        static UnityRandom Urand
+        {
+            get
+            {
+                if (urand != null) return urand;
+                urand = new UnityRandom();
+                return urand;
+            }
+        }
+        
+        /// <summary>
+        /// 0 - 1
+        /// </summary>
+        public static float Value()
+        {
+            return Urand.Value();
+        }
+
+        /// <summary>
+        /// 0 - 1
+        /// </summary>
+        public static float Value(UnityRandom.Normalization n, float t)
+        {
+            return Urand.Value(n, t);
+        }
+        
+        /// <summary>
+        /// min < x < max
+        /// </summary>
+        public static float Range(int minValue, int maxValue)
+        {
+            return Urand.Range(minValue, maxValue);
+        }
+
+        /// <summary>
+        /// min < x < max
+        /// </summary>
+        public static float Range(int minValue, int maxValue, UnityRandom.Normalization n, float t)
+        {
+            return Urand.Range(minValue, maxValue, n, t);
+        }
+
+        /// <summary>
+        /// ポアソン分布
+        /// </summary>
+        public static float Possion(float lambda)
+        {
+            return Urand.Possion(lambda);
+        }
+
+        /// <summary>
+        /// 指数分布
+        /// </summary>
+        public static float Exponential(float lambda)
+        {
+            return Urand.Exponential(lambda);
+        }
+
+        /// <summary>
+        /// ガンマ分布
+        /// </summary>
+        public static float Gamma(float order)
+        {
+            return Urand.Gamma(order);
+        }
+
+        /// <summary>
+        /// L = 1 の正方形の中のランダムな点を返す
+        /// </summary>
+        public static Vector2 PointInASquare()
+        {
+            return Urand.PointInASquare();
+        }
+
+        /// <summary>
+        /// L = 1 の正方形の中のランダムな点を返す
+        /// </summary>
+        public static Vector2 PointInASquare(UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointInASquare(n, t);
+        }
+
+        /// <summary>
+        /// R = 1 の円の中のランダムな点を返す
+        /// </summary>
+        public static Vector2 PointInACircle()
+        {
+            return Urand.PointInACircle();
+        }
+
+        /// <summary>
+        /// R = 1 の円の中のランダムな点を返す
+        /// </summary>
+        public static Vector2 PointInACircle(UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointInACircle(n, t);
+        }
+
+        /// <summary>
+        /// R = 1 のディスクの中のランダムな点を返す
+        /// </summary>
+        public static Vector2 PointInADisk()
+        {
+            return Urand.PointInADisk();
+        }
+
+        /// <summary>
+        /// R = 1 のディスクの中のランダムな点を返す
+        /// </summary>
+        public static Vector2 PointInADisk(UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointInADisk(n, t);
+        }
+
+        /// <summary>
+        /// L = 1 の立方体の中のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointInACube()
+        {
+            return Urand.PointInACube();
+        }
+
+        /// <summary>
+        /// L = 1 の立方体の中のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointInACube(UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointInACube(n, t);
+        }
+
+        /// <summary>
+        /// L = 1 の立方体の表面のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointOnACube()
+        {
+            return Urand.PointOnACube();
+        }
+
+        /// <summary>
+        /// L = 1 の立方体の表面のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointOnACube(UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointOnACube(n, t);
+        }
+
+        /// <summary>
+        /// R = 1 の球体の表面のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointOnASphere()
+        {
+            return Urand.PointOnASphere();
+        }
+
+        /// <summary>
+        /// R = 1 の球体の表面のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointOnASphere(UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointOnASphere(n, t);
+        }
+
+        /// <summary>
+        /// R = 1 の球体の中のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointInASphere()
+        {
+            return Urand.PointInASphere();
+        }
+
+        /// <summary>
+        /// R = 1 の球体の中のランダムな点を返す
+        /// </summary>
+        public static Vector3 PointInASphere(UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointInASphere(n, t);
+        }
+
+        /// <summary>
+        /// 指定された角度のコーンの表面のランダムな点を返す(多分)
+        /// </summary>
+        public static Vector3 PointOnCone(float spotAngle)
+        {
+            return Urand.PointOnCap(spotAngle);
+        }
+
+        /// <summary>
+        /// 指定された角度のコーンの表面のランダムな点を返す(多分)
+        /// </summary>
+        public static Vector3 PointOnCone(float spotAngle, UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointOnCap(spotAngle, n, t);
+        }
+
+        /// <summary>
+        /// 扇の表面のランダムな点を返す(多分)
+        /// </summary>
+        public static Vector3 PointOnRing(float innerAngle, float outerAngle)
+        {
+            return Urand.PointOnRing(innerAngle, outerAngle);
+        }
+
+        /// <summary>
+        /// 扇の表面のランダムな点を返す(多分)
+        /// </summary>
+        public static Vector3 PointOnRing(float innerAngle, float outerAngle, UnityRandom.Normalization n, float t)
+        {
+            return Urand.PointOnRing(innerAngle, outerAngle, n, t);
+        }
+
+        /// <summary>
+        /// ランダムな色を返す
+        /// </summary>
+        public static Color Rainbow()
+        {
+            return Urand.Rainbow();
+        }
+
+        /// <summary>
+        /// ランダムな色を返す
+        /// </summary>
+        public static Color Rainbow(UnityRandom.Normalization n, float t)
+        {
+            return Urand.Rainbow(n, t);
+        }
+
+        /// <summary>
+        /// ダイスロール
+        /// </summary>
+        /// <param name="size">個数</param>
+        public static DiceRoll RollDice(int size, DiceRoll.DiceType type)
+        {
+            return Urand.RollDice(size, type);
+        }
+        
+        /// <summary>
+        /// 配列をシャッフルして返す
+        /// </summary>
+        public static ShuffleBagCollection<float> ShuffleBag(float[] values)
+        {
+            return Urand.ShuffleBag(values);
+        }
+
+        /// <summary>
+        /// 配列をウェイト付きシャッフルして返す。
+        /// </summary>
+        public static ShuffleBagCollection<float> ShuffleBag(Dictionary<float, int> dict)
+        {
+            return Urand.ShuffleBag(dict);
+        }
+        #endregion
     }
 }

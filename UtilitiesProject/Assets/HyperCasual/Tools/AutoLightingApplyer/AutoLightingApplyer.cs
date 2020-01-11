c1d9c39900fbce515e961d865a5ce447e86cb5b4
Kazuki Kaneko <k_kaneko@geishatokyo.com>
2020-01-11 16:32:43 +0900

    パッケージ更新

---
+++ UtilitiesProject/Assets/HyperCasual/Tools/AutoLightingApplyer/AutoLightingApplyer.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class AutoLightingApplyer : MonoBehaviour {
+
+    public bool AutoDestroy = true;
+
+    public bool enableDirLight = true;
+    public Color DirLightColor = Color.white;
+
+    public bool enableClearColor = true;
+    public int selection = 0;
+    public Material SkyBoxMaterial = null;
+    public Color SolidClearColor = Color.black;
+
+    public bool enableFog = false;
+    public FogMode FogMode = FogMode.Exponential;
+    public Color FogColor = Color.white;
+    public float FogDensity = 0.01f;
+    public float FogStart = 0;
+    public float FogEnd = 300;
+
+    public void Store()
+    {
+        this.DirLightColor = FindObjectsOfType<Light>()
+            .First(o => o.type == LightType.Directional)
+            .color;
+        this.selection = Camera.main.clearFlags == CameraClearFlags.Skybox ? 0 : 1;
+        this.SkyBoxMaterial = RenderSettings.skybox;
+        this.SolidClearColor = RenderSettings.ambientSkyColor;
+        this.enableFog = RenderSettings.fog;
+        this.FogMode = RenderSettings.fogMode;
+        this.FogColor = RenderSettings.fogColor;
+        this.FogStart = RenderSettings.fogStartDistance;
+        this.FogEnd = RenderSettings.fogEndDistance;
+        this.FogDensity = RenderSettings.fogDensity;
+    }
+
+    public void Apply()
+    {
+        Debug.Log("!! : " + gameObject.scene.name);
+        if(enableDirLight)
+        {
+            var dirLight = FindObjectsOfType<Light>()
+                .First(o => o.type == LightType.Directional);
+            dirLight.color = this.DirLightColor;
+        }
+
+        if(enableClearColor)
+        {
+            switch(selection)
+            {
+                case 0: 
+                    Camera.main.clearFlags = CameraClearFlags.Skybox;
+                    RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Skybox;
+                    RenderSettings.skybox = this.SkyBoxMaterial;
+                    break;
+                case 1:
+                    Camera.main.clearFlags = CameraClearFlags.Skybox;
+                    RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Flat;
+                    RenderSettings.ambientSkyColor = this.SolidClearColor;
+                    break;
+            }
+        }
+        if(enableFog)
+        {
+            RenderSettings.fog = this.enableFog;
+            RenderSettings.fogMode = this.FogMode;
+            RenderSettings.fogColor = this.FogColor;
+            RenderSettings.fogDensity = this.FogDensity;
+            RenderSettings.fogStartDistance = this.FogStart;
+            RenderSettings.fogEndDistance = this.FogEnd;
+        }
+
+        //if (AutoDestroy && Application.isEditor) DestroyImmediate(this.gameObject);
+        //if (AutoDestroy && Application.isPlaying) Destroy(this.gameObject);
+    }
+}
---
+++ UtilitiesProject/Assets/HyperCasual/Tools/AutoLightingApplyer/Editor/AutoLightingApplyerInspector.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AutoLightingApplyer))]
+public class AutoLightingApplyerInspector : Editor{
+    
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+        var obj = target as AutoLightingApplyer;
+
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            obj.enableDirLight = EditorGUILayout.BeginToggleGroup("平行光源色", obj.enableDirLight);
+            obj.DirLightColor = EditorGUILayout.ColorField(obj.DirLightColor);
+            EditorGUILayout.EndToggleGroup();
+        }
+
+        using (new EditorGUILayout.HorizontalScope())
+        {
+            obj.enableClearColor = EditorGUILayout.BeginToggleGroup("環境光", obj.enableClearColor);
+            obj.selection = GUILayout.SelectionGrid(obj.selection, new[]{"SkyBox","単色"}, 2);
+            if(obj.selection==0)
+            {
+                obj.SkyBoxMaterial = EditorGUILayout.ObjectField(obj.SkyBoxMaterial, typeof(Material), false) as Material;
+            }
+            else
+            {
+                obj.SolidClearColor = EditorGUILayout.ColorField(obj.SolidClearColor);
+            }
+            EditorGUILayout.EndToggleGroup();
+        }
+
+        {
+            obj.enableFog = EditorGUILayout.BeginToggleGroup("Fog", obj.enableFog);
+            obj.FogColor = EditorGUILayout.ColorField(obj.FogColor);
+            obj.FogMode = (FogMode)EditorGUILayout.EnumPopup(obj.FogMode);
+            if(obj.FogMode == FogMode.Linear)
+            {
+                obj.FogStart = EditorGUILayout.FloatField("Start", obj.FogStart);
+                obj.FogEnd = EditorGUILayout.FloatField("End", obj.FogEnd);
+            }
+            else
+            {
+                obj.FogDensity = EditorGUILayout.FloatField("Denstiy", obj.FogDensity);
+            }
+            EditorGUILayout.EndToggleGroup();
+        }
+
+
+        //GUILayout.Space(20);
+        //obj.AutoDestroy = EditorGUILayout.ToggleLeft("AutoDestroy", obj.AutoDestroy);
+
+        GUILayout.Space(10);
+        if(GUILayout.Button("現在のシーンに適用"))
+        {
+            obj.Apply();
+        }
+        if (GUILayout.Button("今の環境設定値をここに入れる"))
+        {
+            obj.Store();
+        }
+
+
+        GUILayout.Space(20);
+
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
